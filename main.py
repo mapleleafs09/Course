221bb6caea7d5b8020018957b9e7876dc05bce2e
Boksha Dmitry <mapleleafs09@gmail.com>
2022-01-10 21:54:23 +0300

    pravki jan 10

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-TOKEN_VK = ''
-TOKEN_DISK = ''
+
 from tqdm import tqdm
 import requests
 import json
-class Upload:
+class VkClient:
     url_vk = 'https://api.vk.com/method/'
-    url_disl = 'https://cloud-api.yandex.net/v1/disk/resources'
 
-    def __init__(self, token_vk, version, token_disk):
+
+    def __init__(self, token_vk, version):
         self.params = {'access_token': token_vk, 'v': version}
-        self.token_disk = token_disk
 
-    def get_photos(self, owner_id,count, album_id='profile',extended='1' ):
+    def id_get(self, user_id):
+
+        method_url = self.url_vk + 'users.get'
+        params = { 'fields': 'last_seen', 'user_ids': user_id }
+        res = requests.get(method_url, params={**self.params, **params})
+        return res.json()['response'][0]['id']
+
+    def get_photos(self, owner_id, count, album_id='profile',extended='1' ):
+        if count == '':
+            count = 5
         method_url = self.url_vk + 'photos.get'
-        params = { 'owner_id': owner_id, 'album_id': album_id, 'extended' : extended, 'count': str(count) }
+        if type(user_id) == str:
+            params = {'owner_id': self.id_get(owner_id), 'album_id': album_id, 'extended': extended, 'count': count}
+        else:
+            params = { 'owner_id': owner_id, 'album_id': album_id, 'extended' : extended, 'count': str(count) }
         res = requests.get(method_url, params={**self.params, **params})
         photo_dict = {}
         for photo in res.json()['response']['items']:
@@ -43,6 +53,13 @@ class Upload:
             json.dump(json_data, f, ensure_ascii=False, indent=2)
         return photo_dict
 
+class DiskClient:
+
+    url_disl = 'https://cloud-api.yandex.net/v1/disk/resources'
+
+    def __init__(self, token_disk):
+        self.token_disk = token_disk
+
     def get_headers(self):
         return {'Content-Type': 'application/json', 'Authorization': 'OAuth {}'.format(self.token_disk)}
 
@@ -53,20 +70,29 @@ class Upload:
         res = requests.put(url, headers=headers, params=params)
         return res
 
-    def post_upload(self, user_id, count=5):
+    def post_upload(self, user_id, count):
         self.put_folder(user_id)
         url = self.url_disl + '/upload'
         headers = self.get_headers()
 
-        for photo_likes, photo_data in tqdm(self.get_photos(user_id, count).items()):
+        for photo_likes, photo_data in tqdm(vk_client.get_photos(user_id, count).items()):
 
             params = {'path': str(user_id) + '/' + str(photo_likes), 'url': photo_data[0]}
             res = requests.post(url, headers=headers, params=params).json()
         print('Фотографии успешно загружены на Яндекс.Диск')
 
 
-upload_client = Upload(TOKEN_VK, '5.131', TOKEN_DISK)
+
+if __name__ == "__main__":
+
+    TOKEN_VK = ''
+    TOKEN_DISK = ''
+
+    vk_client = VkClient(TOKEN_VK, '5.131')
+    disk_client = DiskClient(TOKEN_DISK)
+
+    user_id = input('Введите id пользователя: ')
+    count = input('Введите количество загружаемых фото (enter, если значение по умолчанию): ')
 
 
-user_id = 1
-upload_client.post_upload(user_id,20)
+    disk_client.post_upload(user_id, count)
